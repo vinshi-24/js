01d0e9064cf27fb9af650e7efd21db66942bc92e
vinshi-24 <kminchu918@gmail.com>
2025-06-14 10:55:25 +0000

    maths and numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 10
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.896
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//**************
+// **Maths**************** */
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(6.9));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(1.3));
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(34,5,6,7));
+
+console.log(Math.random()); //returns value between 0 and 1
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
