15fbb48943165cbe980a84e432345147f2242886
vinshi-24 <kminchu918@gmail.com>
2025-06-14 11:25:21 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,25 @@
+// Dates
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("06-14-2025")
+//console.log(myCreatedDate.toLocaleString());
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
+
+
